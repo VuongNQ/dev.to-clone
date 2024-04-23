d37cb26e14511276d5e067784edf116af5a033d7
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2024-04-23 13:16:13 +0700

    fix postcasts

--- front-end-react-typescript/src/features/podcatsts/podcasts.tsx
+++ front-end-react-typescript/src/features/podcatsts/podcasts.tsx
@@ -1,3 +1,4 @@
+import Header from "@/component/UI/GlobalStyle/defaultLayout.tsx/header/header";
 import styles from "./postcasts.module.scss";
 import classNames from "classnames/bind";
 
@@ -5,7 +6,10 @@ const cx = classNames.bind(styles)
 
 function Podcasts() {
   return (
-    <></>
+    <div className={cx('wrapper')}>
+      <h1>Alo alo</h1>
+      <Header />
+    </div>
   );
 }
 
